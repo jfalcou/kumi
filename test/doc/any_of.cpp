70275e1d9ae3847faaef417b3c6fb11819622597
Joel Falcou <joel.falcou@lri.fr>
2022-11-02 18:07:04 +0100

    Predicates now works on non-tuples

--- test/doc/any_of.cpp
+++ test/doc/any_of.cpp
@@ -10,4 +10,5 @@ int main()
 {
   auto t = kumi::tuple{1,2.,3.f};
   std::cout << std::boolalpha << kumi::any_of( t, [](auto e) { return e == 2; }) << "\n";
+  std::cout << std::boolalpha << kumi::any_of( 5.f, [](auto e) { return e == 5; }) << "\n";
 }
