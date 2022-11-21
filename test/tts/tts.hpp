23d007d4eeae6ed0ee3f29b22c12c175013d28d3
Joel Falcou <joel.falcou@lri.fr>
2022-11-21 20:17:07 +0100

    Fix some warning setup

--- test/tts/tts.hpp
+++ test/tts/tts.hpp
@@ -448,7 +448,7 @@ namespace tts::detail
       return test::acknowledge( { tg.name
                                 , [tg,body]() mutable
                                   {
-                                    std::mt19937 gen(::tts::random_seed());
+                                    std::mt19937 gen(static_cast<std::mt19937::result_type>(::tts::random_seed()));
                                     ( ( (current_type = " with [T = " + typename_<Types> + "]")
                                       , std::apply(body, tg.generator(type<Types>{}, gen))
                                       ), ...
@@ -622,37 +622,45 @@ namespace tts
 #define TTS_CEXPR_EXPECT_(EXPR)         TTS_CEXPR_EXPECT_IMPL(EXPR,TTS_FAIL)
 #define TTS_CEXPR_EXPECT_REQUIRED(EXPR) TTS_CEXPR_EXPECT_IMPL(EXPR,TTS_FATAL)
 #define TTS_CEXPR_EXPECT_IMPL(EXPR,FAILURE)                                                         \
-[&]()                                                                                               \
+::tts::global_logger_status = false;                                                                \
+do                                                                                                  \
 {                                                                                                   \
-  using result_tts = std::bool_constant<EXPR>;                                                      \
-  if constexpr( result_tts::value )                                                                 \
+  constexpr auto result_tts = EXPR;                                                                 \
+  if( result_tts )                                                                                  \
   {                                                                                                 \
-    ::tts::global_runtime.pass(); return ::tts::logger{false};                                      \
+    ::tts::global_runtime.pass();                                                                   \
+    ::tts::global_logger_status = false;                                                            \
   }                                                                                                 \
   else                                                                                              \
   {                                                                                                 \
-    FAILURE ( "Expression: "  << TTS_STRING(EXPR) << " evaluates to true." );                       \
-    return ::tts::logger{};                                                                         \
+    FAILURE ( "Expression: "  << TTS_STRING(EXPR) << " evaluates to false." );                      \
+    ::tts::global_logger_status = true;                                                             \
   }                                                                                                 \
-}()                                                                                                 \
+}while(0);                                                                                          \
+::tts::logger{::tts::global_logger_status}                                                          \
+/**/
 
 #define TTS_CONSTEXPR_EXPECT_NOT(EXPR, ...) TTS_CEXPR_EXPECT_NOT_ ## __VA_ARGS__ ( EXPR )
 #define TTS_CEXPR_EXPECT_NOT_(EXPR)         TTS_CEXPR_EXPECT_NOT_IMPL(EXPR,TTS_FAIL)
 #define TTS_CEXPR_EXPECT_NOT_REQUIRED(EXPR) TTS_CEXPR_EXPECT_NOT_IMPL(EXPR,TTS_FATAL)
 #define TTS_CEXPR_EXPECT_NOT_IMPL(EXPR,FAILURE)                                                     \
-[&]()                                                                                               \
+::tts::global_logger_status = false;                                                                \
+do                                                                                                  \
 {                                                                                                   \
-  using result_tts = std::bool_constant<EXPR>;                                                      \
-  if constexpr( !result_tts::value )                                                                \
+  constexpr auto result_tts = EXPR;                                                                 \
+  if( !result_tts )                                                                                 \
   {                                                                                                 \
-    ::tts::global_runtime.pass(); return ::tts::logger{false};                                      \
+    ::tts::global_runtime.pass();                                                                   \
+    ::tts::global_logger_status = false;                                                            \
   }                                                                                                 \
   else                                                                                              \
   {                                                                                                 \
     FAILURE ( "Expression: "  << TTS_STRING(EXPR) << " evaluates to true." );                       \
-    return ::tts::logger{};                                                                         \
+    ::tts::global_logger_status = true;                                                             \
   }                                                                                                 \
-}()                                                                                                 \
+}while(0);                                                                                          \
+::tts::logger{::tts::global_logger_status}                                                          \
+/**/                                                                                                \
 
 #include <cstring>
 #include <array>
