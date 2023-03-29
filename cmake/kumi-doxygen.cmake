2f05f14387a946c20ff4d4a9f6ed6f083576a8f0
Joel Falcou <joel.falcou@lri.fr>
2023-03-29 07:43:11 +0200

    Simplify doxygen target in CMake

--- cmake/kumi-doxygen.cmake
+++ cmake/kumi-doxygen.cmake
@@ -15,16 +15,10 @@ else (DOXYGEN_FOUND)
   message( STATUS "[kumi] Doxygen need to be installed to generate the doxygen documentation")
 endif (DOXYGEN_FOUND)
 
+
 if (DOXYGEN_FOUND)
   set(DOXYGEN_CONFIG ${PROJECT_SOURCE_DIR}/doc/Doxyfile)
   add_custom_target ( doxygen
-    COMMAND KUMI_DOXYGEN_OUPUT=../docs ${DOXYGEN_EXECUTABLE} ${DOXYGEN_CONFIG}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/doc
-    COMMENT "[kumi] Generating API documentation with Doxygen"
-    VERBATIM
-  )
-
-  add_custom_target ( doxygen-local
                       COMMAND KUMI_DOXYGEN_OUPUT=${PROJECT_BINARY_DIR}/docs ${DOXYGEN_EXECUTABLE} ${DOXYGEN_CONFIG}
                       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/doc
                       COMMENT "[kumi] Generating API documentation with Doxygen - Local version"
