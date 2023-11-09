b2c9fc370f4daa0978d9c8b8fe143f9f1873a8be
jfalcou <jfalcou@users.noreply.github.com>
2023-11-09 20:05:24 +0000

    deploy: a2a9dcd95b13a4fe5c5044712984838056e88840

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['make_5ftuple_0',['make_tuple',['../group__tuple_ga7a585805b36bec6b2c275b6f2ced67fe.html#ga7a585805b36bec6b2c275b6f2ced67fe',1,'kumi::tuple']]],
-  ['map_1',['map',['../group__transforms_gaeeddccfe55a8c97260599a295d38d875.html#gaeeddccfe55a8c97260599a295d38d875',1,'kumi']]],
+  ['map_1',['map',['../group__transforms_ga8a4e424cfc506fb8fd2939ebe24382ce.html#ga8a4e424cfc506fb8fd2939ebe24382ce',1,'kumi']]],
   ['map_5findex_2',['map_index',['../group__transforms_ga24c0a7632bd20e448dd7ac2d18e1168c.html#ga24c0a7632bd20e448dd7ac2d18e1168c',1,'kumi']]],
   ['max_3',['max',['../group__reductions_ga7cb6c22dc5a7e603a051591859e8ccee.html#ga7cb6c22dc5a7e603a051591859e8ccee',1,'kumi::max(T const &amp;t) noexcept'],['../group__reductions_ga5e11e6a667c808afc39641a25d5190c5.html#ga5e11e6a667c808afc39641a25d5190c5',1,'kumi::max(T const &amp;t, F f) noexcept']]],
   ['max_5fflat_4',['max_flat',['../group__reductions_ga7398e59d84beec31000bf3d6bbe28ec6.html#ga7398e59d84beec31000bf3d6bbe28ec6',1,'kumi']]],
