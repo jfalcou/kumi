3396187cb383f806499e22ecb56e43c8c985779a
John Eivind Helset <jehelset@gmail.com>
2023-11-19 12:42:05 +0100

    Fix #77 - Add missing forward in cat.

--- test/unit/cat.cpp
+++ test/unit/cat.cpp
@@ -30,6 +30,26 @@ TTS_CASE("Check result::cat<Tuple...> behavior")
   TTS_TYPE_IS ( (kumi::result::cat_t<kumi::tuple<char,short&>,kumi::tuple<int&,double>>)
               , (kumi::tuple<char,short&,int&,double>)
               );
+
+  TTS_TYPE_IS ( (kumi::result::cat_t<kumi::tuple<char &&> &&>)
+              , (kumi::tuple<char &&>)
+              );
+
+  TTS_TYPE_IS ( (kumi::result::cat_t<kumi::tuple<char &&> &&, kumi::tuple<char &> &, kumi::tuple<char> &>)
+              , (kumi::tuple<char &&, char &, char>)
+              );
+
+  TTS_TYPE_IS ( (kumi::result::cat_t<kumi::tuple<char &&> &&, kumi::tuple<char &> &&, kumi::tuple<char> &>)
+                , (kumi::tuple<char &&, char &, char>)
+              );
+
+  TTS_TYPE_IS ( (kumi::result::cat_t<kumi::tuple<char &&> &&, kumi::tuple<char &> &, kumi::tuple<char> &&>)
+              , (kumi::tuple<char &&, char &, char>)
+              );
+
+  TTS_TYPE_IS ( (kumi::result::cat_t<kumi::tuple<char &&> &&, kumi::tuple<char &> &&, kumi::tuple<char> &&>)
+                , (kumi::tuple<char &&, char &, char>)
+              );
 };
 
 TTS_CASE("Check cat(tuple) behavior")
@@ -44,10 +64,19 @@ TTS_CASE("Check cat(tuple) behavior")
             (kumi::tuple {1, 2., 3.f, 4, s, 6.7}));
 
   // Check behavior with tuple of references
-  auto ref = kumi::tie(s);
-  auto val = kumi::tuple<float const>{3.14f};;
+  {
+    auto ref = kumi::tie(s);
+    auto val = kumi::tuple<float const>{3.14f};
 
-  TTS_EQUAL(kumi::cat(ref, val), (kumi::tuple<short&,float const>{s,3.14f}) );
+    TTS_EQUAL(kumi::cat(ref, val), (kumi::tuple<short&,float const>{s,3.14f}) );
+  }
+  {
+    auto ref = kumi::tie(s);
+    auto rref = kumi::tuple<short &&>{std::move(s)};
+    auto val = kumi::tuple<float const>{3.14f};
+
+    TTS_EQUAL(kumi::cat(ref, std::move(rref), val), (kumi::tuple<short&, short &&, float const>{s,std::move(s),3.14f}) );
+  }
 };
 
 TTS_CASE("Check cat(tuple) constexpr behavior")
