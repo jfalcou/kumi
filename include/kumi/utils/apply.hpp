c6c63af62d67cc868328c7312c23e42ab8b30412
jehelset <47066444+jehelset@users.noreply.github.com>
2024-05-30 20:24:24 +0200

    Fix #81 - Add support for `std::invoke`-like calls in `apply`.
    Also adds noexcept spec.

--- include/kumi/utils/apply.hpp
+++ include/kumi/utils/apply.hpp
@@ -9,6 +9,32 @@
 
 namespace kumi
 {
+
+  namespace _{
+    template<typename T>
+    inline constexpr bool is_reference_wrapper_v =
+      !std::is_same_v<std::decay_t<typename std::unwrap_reference<T &&>::type>,
+                      typename std::unwrap_ref_decay<T &&>::type>;
+
+    template<typename T>
+    struct apply_object_unwrap{
+      using type = T &&;
+    };
+    template<typename T>
+    requires is_reference_wrapper_v<T>
+    struct apply_object_unwrap<T>{
+      using type = typename std::remove_cvref_t<T>::type &;
+    };
+    template<typename T>
+    requires std::is_pointer_v<std::remove_cvref_t<T>>
+    struct apply_object_unwrap<T>{
+      using type = std::remove_pointer_t<std::remove_cvref_t<T>> &;
+    };
+    template<typename T>
+    using apply_object_unwrap_t = typename apply_object_unwrap<T>::type;
+
+  }
+
   //================================================================================================
   //! @ingroup transforms
   //! @brief Invoke the Callable object f with a tuple of arguments.
@@ -34,18 +60,32 @@ namespace kumi
   //! @include doc/apply.cpp
   //================================================================================================
   template<typename Function, product_type Tuple>
-  constexpr decltype(auto) apply(Function &&f, Tuple &&t)
+  constexpr decltype(auto) apply(Function &&f, Tuple &&t) noexcept(_::supports_nothrow_apply<Function &&, Tuple &&>)
   requires _::supports_apply<Function&&, Tuple&&>
   {
-    if constexpr(sized_product_type<Tuple,0>) return  KUMI_FWD(f)();
+    if constexpr(sized_product_type<Tuple,0>) return KUMI_FWD(f)();
+    else if constexpr (std::is_member_pointer_v<std::decay_t<Function>>)
+      return [&]<std::size_t... I>(std::index_sequence<I...>) -> decltype(auto){
+        auto &&w = [](auto &&y) -> decltype(auto){
+          if constexpr(_::is_reference_wrapper_v<decltype(y)>)
+            return y.get();
+          else if constexpr(std::is_pointer_v<std::remove_cvref_t<decltype(y)>>)
+            return *y;
+          else
+            return KUMI_FWD(y);
+        }(get<0>(KUMI_FWD(t)));
+        if constexpr(std::is_member_object_pointer_v<std::remove_cvref_t<decltype(f)>>)
+          return KUMI_FWD(w).*f;
+        else
+          return (KUMI_FWD(w).*f)(get<I + 1>(KUMI_FWD(t))...);
+      }
+      (std::make_index_sequence<size<Tuple>::value - 1>());
     else
-    {
       return [&]<std::size_t... I>(std::index_sequence<I...>) -> decltype(auto)
       {
         return KUMI_FWD(f)(get<I>(KUMI_FWD(t))...);
       }
       (std::make_index_sequence<size<Tuple>::value>());
-    }
   }
 
   namespace result
