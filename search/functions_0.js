aeac7b755be8f9c70a6306bca3ed77724174da10
jfalcou <jfalcou@users.noreply.github.com>
2024-05-30 18:26:43 +0000

    deploy: c6c63af62d67cc868328c7312c23e42ab8b30412

--- search/functions_0.js
+++ search/functions_0.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['all_5fof_0',['all_of',['../group__queries_ga262f5e099c46aacaf7b117033b22b7e8.html#ga262f5e099c46aacaf7b117033b22b7e8',1,'kumi::all_of(T const &amp;ts, Pred p) noexcept'],['../group__queries_ga40bf2969c7b8a6fdf5c8eecf6a1436a2.html#ga40bf2969c7b8a6fdf5c8eecf6a1436a2',1,'kumi::all_of(T const &amp;ts) noexcept']]],
   ['any_5fof_1',['any_of',['../group__queries_ga8d7de5c00a3b6cfc1fc4a8ed3bea53b2.html#ga8d7de5c00a3b6cfc1fc4a8ed3bea53b2',1,'kumi::any_of(T const &amp;ts, Pred p) noexcept'],['../group__queries_gafdfb5550011004d4306d9c8a67586d66.html#gafdfb5550011004d4306d9c8a67586d66',1,'kumi::any_of(T const &amp;ts) noexcept']]],
-  ['apply_2',['apply',['../group__transforms_ga04cb7535442ad14350e95af08e6880a1.html#ga04cb7535442ad14350e95af08e6880a1',1,'kumi']]],
+  ['apply_2',['apply',['../group__transforms_ga612ed868d8ee0ecabcdaa0d6a7c116fb.html#ga612ed868d8ee0ecabcdaa0d6a7c116fb',1,'kumi']]],
   ['as_5fflat_5fptr_3',['as_flat_ptr',['../group__generators_ga592ec585976cc6544c799402a5c4ef1c.html#ga592ec585976cc6544c799402a5c4ef1c',1,'kumi']]]
 ];
