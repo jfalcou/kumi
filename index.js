24411f97018f812f677208b8ccfcd8a02d8cbe7f
jfalcou <jfalcou@users.noreply.github.com>
2024-11-11 09:13:40 +0000

    deploy: 1019acc2bc84ea2b8ffd35607afe58101c93cac6

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var index =
 [
-    [ "A Short Example", "index.html#autotoc_md129", null ],
-    [ "Licence", "index.html#autotoc_md130", null ]
+    [ "A Short Example", "index.html#autotoc_md134", null ],
+    [ "Licence", "index.html#autotoc_md135", null ]
 ];
