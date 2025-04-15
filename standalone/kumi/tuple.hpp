5fd61afd7e2b0cde89e5c09acabbad9d14af8dea
Nxirda <93188097+Nxirda@users.noreply.github.com>
2025-04-15 11:08:33 +0200

    Implement map_traits
    Co-authored-by: Nxirda <Nxirda@users.noreply.github.com>
    Co-authored-by: Joel Falcou <joel.falcou@lri.fr>

--- standalone/kumi/tuple.hpp
+++ standalone/kumi/tuple.hpp
@@ -859,27 +859,6 @@ namespace kumi
 }
 namespace kumi
 {
-  template< template<typename...> typename Traits
-          , product_type Tuple
-          , typename Seq = std::make_index_sequence<size<Tuple>::value>
-          >
-  struct apply_traits;
-  template< template<typename...> typename Traits
-          , product_type Tuple
-          , std::size_t... Is
-          >
-  requires( requires {typename Traits<element_t<Is,Tuple>...>::type;})
-  struct  apply_traits<Traits, Tuple, std::index_sequence<Is...>>
-  {
-    using type = typename Traits<element_t<Is,Tuple>...>::type;
-  };
-  template< template<typename...> typename Traits
-          , product_type Tuple
-          >
-  using apply_traits_t = typename apply_traits<Traits, Tuple>::type;
-}
-namespace kumi
-{
 }
 namespace kumi
 {
@@ -1952,6 +1931,45 @@ namespace kumi
     template<product_type Tuple>
     using transpose_t = typename transpose<Tuple>::type;
   }
+}
+namespace kumi
+{
+  template< template<typename...> typename Traits
+          , product_type Tuple
+          , typename Seq = std::make_index_sequence<size<Tuple>::value>
+          >
+  struct apply_traits;
+  template< template<typename...> typename Traits
+          , product_type Tuple
+          , std::size_t... Is
+          >
+  requires( requires {typename Traits<element_t<Is,Tuple>...>::type;})
+  struct  apply_traits<Traits, Tuple, std::index_sequence<Is...>>
+  {
+    using type = typename Traits<element_t<Is,Tuple>...>::type;
+  };
+  template< template<typename...> typename Traits
+          , product_type Tuple
+          >
+  using apply_traits_t = typename apply_traits<Traits, Tuple>::type;
+  template< template<typename...> typename Traits
+          , product_type Tuple
+          , typename Seq = std::make_index_sequence<size<Tuple>::value>
+          >
+  struct map_traits;
+  template< template<typename...> typename Traits
+          , product_type Tuple
+          , std::size_t... Is
+          >
+  requires( requires {typename Traits<element_t<Is,Tuple>>::type;} && ...)
+  struct  map_traits<Traits, Tuple, std::index_sequence<Is...>>
+  {
+    using type = tuple <typename Traits<element_t<Is, Tuple>>::type...>;
+  };
+  template< template<typename...> typename Traits
+          , product_type Tuple
+          >
+  using map_traits_t = typename map_traits<Traits, Tuple>::type;
 }
 namespace kumi
 {
