195729361467654dc24598082761a2cb03e10570
Joel Falcou <joel.falcou@lri.fr>
2025-08-09 10:36:41 +0200

    Rearrange tree links in documentation

--- include/kumi/detail/abi.hpp
+++ include/kumi/detail/abi.hpp
@@ -7,7 +7,10 @@
 //==================================================================================================
 #pragma once
 
-#if defined(__GNUC__)
+#if defined(KUMI_DOXYGEN_INVOKED)
+#   define KUMI_TRIVIAL inline
+#   define KUMI_TRIVIAL_NODISCARD [[nodiscard]] inline
+#elif defined(__GNUC__)
 #   define KUMI_TRIVIAL [[gnu::always_inline, gnu::flatten, gnu::artificial]] inline
 #   define KUMI_TRIVIAL_NODISCARD [[nodiscard, gnu::always_inline, gnu::flatten, gnu::artificial]] inline
 #elif defined(_MSC_VER)
