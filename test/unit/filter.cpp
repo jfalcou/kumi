a934390a1d8e91af18cf77d4bf25c9819cdc66a2
Nxirda <Nxirda@users.noreply.github.com>
2025-08-13 20:01:12 +0200

    Adding kumi::filter and kumi::filter_not

---
+++ test/unit/filter.cpp
@@ -0,0 +1,68 @@
+//==================================================================================================
+/*
+  KUMI - Compact Tuple Tools
+  Copyright : KUMI Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#define TTS_MAIN
+#include <kumi/kumi.hpp>
+#include <tts/tts.hpp>
+#include <type_traits>
+
+TTS_CASE("Check result::filter_t<Func,Tuple> behavior")
+{
+  TTS_TYPE_IS ( (kumi::result::filter_t<std::is_integral,kumi::tuple<>>), (kumi::tuple<>));
+
+  TTS_TYPE_IS ( (kumi::result::filter_t<std::is_integral,kumi::tuple<int,float,double>>)
+              , (kumi::tuple<int>)
+              );
+
+  TTS_TYPE_IS ( (kumi::result::filter_t< std::is_rvalue_reference
+                                          , kumi::tuple<int&,float&&,double const&>
+                                          >
+                )
+              , (kumi::tuple<float&&>)
+              );
+};
+
+TTS_CASE("Check filter(kumi::tuple<>{}) behavior")
+{
+  TTS_EQUAL ( (kumi::filter<std::is_pointer>(kumi::tuple{}))
+            , (kumi::tuple<>{})
+            );
+};
+
+TTS_CASE("Check partition() behavior with values")
+{
+  int     a = 4;
+  double  b = 3.1415;
+  float   c = 0.01f;
+
+  auto original = kumi::tuple{a,&a,b,&b,c,&c,'z',nullptr};
+
+  TTS_EQUAL ( kumi::filter<std::is_pointer>(original)
+            , (kumi::tuple{&a,&b,&c})
+            );
+
+  TTS_EQUAL ( kumi::filter<std::is_floating_point>(original)
+            , (kumi::tuple{b,c})
+            );
+
+  TTS_EQUAL ( kumi::filter<std::is_null_pointer>(original)
+            , (kumi::tuple{nullptr})
+            );
+};
+
+
+TTS_CASE("Check filter() behavior with reference")
+{
+  int     a = 4;
+  double  b = 3.1415;
+
+  auto original = kumi::tuple<int,int&,double,double&>{a,a,b,b};
+
+  TTS_EQUAL ( kumi::filter<std::is_lvalue_reference>(original)
+            , (kumi::tuple<int&,double&>{a,b})
+            );
+};
---
+++ test/unit/filter_not.cpp
@@ -0,0 +1,70 @@
+//==================================================================================================
+/*
+  KUMI - Compact Tuple Tools
+  Copyright : KUMI Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#define TTS_MAIN
+#include <kumi/kumi.hpp>
+#include <tts/tts.hpp>
+#include <type_traits>
+
+TTS_CASE("Check result::filter_not_t<Func,Tuple> behavior")
+{
+  TTS_TYPE_IS ( (kumi::result::filter_not_t<std::is_integral,kumi::tuple<>>)
+              , (kumi::tuple<>)
+              );
+
+  TTS_TYPE_IS ( (kumi::result::filter_not_t<std::is_integral,kumi::tuple<int,float,double>>)
+              , (kumi::tuple<float,double>)
+              );
+
+  TTS_TYPE_IS ( (kumi::result::filter_not_t< std::is_rvalue_reference
+                                          , kumi::tuple<int&,float&&,double const&>
+                                          >
+                )
+              , (kumi::tuple<int&, double const&>)
+              );
+};
+
+TTS_CASE("Check filter_not(kumi::tuple<>{}) behavior")
+{
+  TTS_EQUAL ( (kumi::filter_not<std::is_pointer>(kumi::tuple{}))
+            , (kumi::tuple<>{})
+            );
+};
+
+TTS_CASE("Check filter_not() behavior with values")
+{
+  int     a = 4;
+  double  b = 3.1415;
+  float   c = 0.01f;
+
+  auto original = kumi::tuple{a,&a,b,&b,c,&c,'z',nullptr};
+
+  TTS_EQUAL ( kumi::filter_not<std::is_pointer>(original)
+            , (kumi::tuple{a,b,c,'z',nullptr})
+            );
+
+  TTS_EQUAL ( kumi::filter_not<std::is_floating_point>(original)
+            , (kumi::tuple{a,&a,&b,&c,'z',nullptr})
+            );
+
+  TTS_EQUAL ( kumi::filter_not<std::is_null_pointer>(original)
+            , (kumi::tuple{a,&a,b,&b,c,&c,'z',})
+            );
+};
+
+
+TTS_CASE("Check filter_not() behavior with reference")
+{
+  int     a = 4;
+  double  b = 3.1415;
+
+  auto original = kumi::tuple<int,int&,double,double&>{a,a,b,b};
+
+  TTS_EQUAL ( kumi::filter_not<std::is_lvalue_reference>(original)
+            , (kumi::tuple{a,b})
+            );
+};
