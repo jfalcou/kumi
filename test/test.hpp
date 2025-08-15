4781ca4002212f0553ebe2add22eaa1f5ddc8bc2
Nxirda <Nxirda@users.noreply.github.com>
2025-08-15 20:06:02 +0200

    Light tweaks, for once it s icpx and macos that broke

--- test/test.hpp
+++ test/test.hpp
@@ -6,9 +6,13 @@
 */
 //==================================================================================================
 
-template<typename T, typename Enable = void> struct is_moveonly_type : std::false_type {};
-template<typename T> struct is_moveonly_type<T, typename T::is_moveonly_type> : std::true_type {};
-template<typename T> struct is_not_moveonly_type : std::bool_constant<!is_moveonly_type<T>::value>{};
+template<typename T, typename Enable = void> struct is_moveonly_i : std::false_type {};
+template<typename T> struct is_moveonly_i<T, typename T::is_moveonly_i> : std::true_type {};
+
+template<typename T> struct is_not_moveonly_i : std::bool_constant<!is_moveonly_i<T>::value>{};
+
+template<typename T> using is_moveonly_type     = is_moveonly_i<std::remove_cvref_t<T>>;
+template<typename T> using is_not_moveonly_type = is_not_moveonly_i<std::remove_cvref_t<T>>;
 
 struct moveonly{
     using is_moveonly                       = void;
