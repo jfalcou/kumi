db7a5f07ff10b27fa655f8b09831926a4afff767
Nxirda <Nxirda@users.noreply.github.com>
2025-08-15 21:44:56 +0200

    Silly test fix

--- include/kumi/algorithm/zip.hpp
+++ include/kumi/algorithm/zip.hpp
@@ -37,9 +37,11 @@ namespace kumi
   [[nodiscard]] KUMI_ABI constexpr auto zip(T0 &&t0, Ts &&...ts)
   requires ( compatible_product_types<T0, Ts...> )
   {
-    return kumi::map( [](auto &&m0, auto &&...ms) 
+    return kumi::map( []<typename M, typename... Ms>(M&& m0, Ms&&...ms) 
                     { 
-                        return kumi::make_tuple(KUMI_FWD(m0), KUMI_FWD(ms)...);
+                        //using type = _::builder_make_t<kumi::tuple<>, M, Ms...>;
+                        return _::builder<kumi::tuple<>>::make(KUMI_FWD(m0), KUMI_FWD(ms)...);
+                        //return kumi::make_tuple(KUMI_FWD(m0), KUMI_FWD(ms)...);
                     }
                     , KUMI_FWD(t0),KUMI_FWD(ts)...
                     );
