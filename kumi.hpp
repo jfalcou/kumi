04b4d65499a0cf39fdd41087a9cff8698b38a298
KUMI Standalone Generator <kumi@nowhere.com>
2025-11-04 18:09:27 +0000

    Latest KUMI standalone

--- kumi.hpp
+++ kumi.hpp
@@ -9,6 +9,22 @@
 namespace kumi
 {
 }
+namespace kumi 
+{
+  template<typename T>
+  struct as
+  {
+    using type = T;
+             constexpr as()         noexcept {}
+    explicit constexpr as(T const&) noexcept {}
+  };
+  template<>
+  struct as<void>
+  {
+    using type = void;
+    constexpr as() noexcept {}
+  };
+}
 #if defined(__clang__)
 #  pragma clang diagnostic ignored "-Wmissing-braces"
 #endif
@@ -153,7 +169,7 @@ namespace kumi
     template <typename... Us>
     static auto is_set(Us...) -> decltype(_::true_fn(static_cast<Us>(all_uniques_inner())...));
     static std::false_type is_set(...);
-    using type = decltype(is_set(Ts{}...));
+    using type = decltype(is_set(as<Ts>{}...));
   };
   template<typename... Ts>
   using all_uniques_t = typename all_uniques<std::index_sequence_for<Ts...>, Ts...>::type;
@@ -170,7 +186,7 @@ namespace kumi
     template <typename... Us>
     static auto is_set(Us...) -> decltype(_::true_fn(static_cast<Us>(all_uniques_inner())...));
     static std::false_type is_set(...);
-    using type = decltype(is_set(Ts{}...));
+    using type = decltype(is_set(as<Ts>{}...));
   };
   template<typename... Ts>
   using all_unique_names_t = typename all_unique_names<std::index_sequence_for<Ts...>, Ts...>::type;
@@ -1608,8 +1624,10 @@ namespace kumi
     using is_product_type   = void;
     using is_record_type    = void;
     static constexpr bool is_homogeneous = false;
-    static constexpr auto size()  noexcept { return std::size_t{0}; }
-    static constexpr auto empty() noexcept { return true;           }
+    static constexpr auto size()    noexcept { return std::size_t{0}; }
+    static constexpr auto empty()   noexcept { return true;           }
+    static constexpr auto names()   noexcept { return tuple{};        }
+    static constexpr auto values()  noexcept { return tuple{};        }
     KUMI_ABI friend constexpr auto operator<=>(record<>, record<>) noexcept = default;
     friend std::ostream& operator<<(std::ostream& os, record<>)
     {
@@ -2368,7 +2386,7 @@ namespace kumi
 namespace kumi
 {
   template<product_type Tuple, typename T>
-  [[nodiscard]] KUMI_ABI constexpr auto push_front(Tuple && t, T&& v)
+  [[nodiscard]] KUMI_ABI constexpr auto push_front(Tuple && t, T && v)
   {
     return [&]<std::size_t... I>(std::index_sequence<I...>)
     {
@@ -3102,6 +3120,97 @@ namespace kumi
           , product_type Tuple
           >
   using map_traits_t = typename map_traits<Traits, Tuple>::type;
+}
+namespace kumi
+{
+  namespace _ 
+  {
+    template<typename T> struct make_unique
+    {
+      T acc;
+      template<typename W>
+      KUMI_ABI friend constexpr decltype(auto) operator | (make_unique &&x, make_unique<W> &&y)
+      {
+        constexpr auto value = []<std::size_t...I>(std::index_sequence<I...>)
+        {
+          return (all_uniques_v<W, raw_element_t<I, T>...>);
+        }(std::make_index_sequence<size_v<T>>{});
+        if constexpr ( value ) return [&]<std::size_t...I>(std::index_sequence<I...>)
+        {
+           using res_t = _::builder_make_t<T, element_t<I,T>..., W>;
+           return _::make_unique{ res_t{ get<I>(KUMI_FWD(x.acc))..., KUMI_FWD(y.acc) } };
+        }(std::make_index_sequence<size_v<T>>{});
+        else                   return KUMI_FWD(x);
+      }
+    };
+    template<typename W> make_unique(W && w) -> make_unique<W>;
+    struct uniquable
+    {
+      template<product_type T>
+      [[nodiscard]] KUMI_ABI consteval auto operator()(as<T>) const noexcept
+      {
+        struct { std::size_t count{1}, t[size_v<T>]; } that{};
+        that.t[0] = 0; 
+        [&]<std::size_t...I>(std::index_sequence<I...>)
+        { 
+          ([&]
+          {
+            constexpr auto L = I;
+            constexpr auto R = I+1;
+            if constexpr ( !std::is_same_v<raw_element_t<L,T>, raw_element_t<R,T>> ) 
+              that.t[that.count++] = R;
+          }(), ...);
+        }(std::make_index_sequence<size_v<T> - 1>{});
+        return that;
+      }
+    };
+    inline constexpr uniquable uniqued{};
+  }
+  template<product_type T>
+  [[nodiscard]] KUMI_ABI constexpr auto unique(T && t)
+  {
+    if constexpr ( sized_product_type<T,0> ) return KUMI_FWD(t);
+    else
+    {  
+      constexpr auto pos = _::uniqued(as<T>{});
+      return [&]<std::size_t...I>(std::index_sequence<I...>)
+      {
+        using ret_t = _::builder_make_t<T, raw_element_t<pos.t[I], T>...>;
+        return ret_t{ get<pos.t[I]>(KUMI_FWD(t))... };
+      }(std::make_index_sequence<pos.count>{});
+    }
+  };
+  template <product_type T>
+  [[nodiscard]] KUMI_ABI constexpr auto all_unique(T && t)
+  {
+    if constexpr ( sized_product_type<T,0> ) return t;
+    else
+    {
+      return [&]<std::size_t...I>(std::index_sequence<I...>)
+      {
+        return ( _::make_unique{_::builder_make_t<T,raw_element_t<0,T>>{ get<0>(KUMI_FWD(t)) }}
+               | ...
+               | _::make_unique<raw_element_t<I+1,T>>{ get<I+1>(KUMI_FWD(t)) }).acc;
+      }(std::make_index_sequence<size_v<T>-1>{});
+    }
+  }
+  namespace result
+  {
+    template<product_type T>
+    struct unique
+    {
+      using type = decltype(kumi::unique(std::declval<T>()));
+    };
+    template<product_type T>
+    struct all_unique
+    {
+      using type = decltype(kumi::all_unique(std::declval<T>())); 
+    };
+    template<product_type T>
+    using unique_t = typename unique<T>::type;
+    template<product_type T>
+    using all_unique_t = typename all_unique<T>::type;
+  }
 }
 namespace kumi
 {
