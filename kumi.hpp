d1bff7244cee2e676b71df561344403fb26005b6
KUMI Standalone Generator <kumi@nowhere.com>
2026-01-16 12:53:10 +0000

    Latest KUMI standalone

--- kumi.hpp
+++ kumi.hpp
@@ -25,22 +25,31 @@ namespace kumi
     constexpr as() noexcept {}
   };
 }
-#if defined(__clang__)
-#  pragma clang diagnostic ignored "-Wmissing-braces"
+#include <cstddef>
+#if defined(_MSC_VER)
+#    if _MSVC_LANG < 202002L
+#        error "Kumi requires C++20 or higher. Use /std:c++20 or higher to enable C++20 features."
+#    endif
+#else
+#    if __cplusplus < 202002L
+#        error "KUMI requires C++20 or higher. Use -std=c++20 or higher to enable C++20 features."
+#    endif
 #endif
 #define KUMI_FWD(...) static_cast<decltype(__VA_ARGS__) &&>(__VA_ARGS__)
-#include <cstddef>
 #if defined(__CUDACC__)
-#define KUMI_CUDA __host__ __device__
+#   define KUMI_CUDA __host__ __device__
 #else
-#define KUMI_CUDA
+#   define KUMI_CUDA
 #endif
 #if defined(KUMI_DEBUG)
 #   define KUMI_ABI
-#elif defined(__GNUC__)
-#   define KUMI_ABI [[gnu::always_inline, gnu::flatten, gnu::artificial]] KUMI_CUDA inline
+#elif defined(__GNUC__) || defined(__clang__)
+#   define KUMI_ABI [[using gnu : always_inline, flatten, artificial]] KUMI_CUDA inline
 #elif defined(_MSC_VER)
-#   define KUMI_ABI KUMI_CUDA __forceinline
+#   define KUMI_ABI [[using msvc : forceinline, flatten]] KUMI_CUDA inline 
+#endif
+#if defined(__clang__)
+#  pragma clang diagnostic ignored "-Wmissing-braces"
 #endif
 #include <iosfwd>
 #include <cstdint>
