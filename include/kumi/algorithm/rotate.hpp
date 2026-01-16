49e53d219dacf588bd0777ab98e785f9054ed4b7
Nxirda <93188097+Nxirda@users.noreply.github.com>
2026-01-16 13:54:55 +0100

    Add rotate algorithm
    Co-authored-by: Nxirda <Nxirda@users.noreply.github.com>

---
+++ include/kumi/algorithm/rotate.hpp
@@ -0,0 +1,139 @@
+//==================================================================================================
+/*
+  KUMI - Compact Tuple Tools
+  Copyright : KUMI Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+namespace kumi
+{
+  namespace _
+  {
+    template<std::size_t S, std::size_t R> struct rotate_t
+    {
+      KUMI_ABI constexpr auto operator()() const noexcept
+      {
+        struct { std::size_t t[1+S]; } that{};
+
+        auto idxs = [&]<std::size_t... I>(std::index_sequence<I...>)
+        {
+          ((that.t[I] = (I + R) % S), ...);
+        };
+
+        idxs(std::make_index_sequence<S>{});
+        return that;
+      }
+    };
+
+    template<std::size_t S, std::size_t R>
+    inline constexpr rotate_t<S,R> rotator{};
+  }
+
+  //================================================================================================
+  //! @ingroup generators 
+  //! @brief  Rotates the element of a product type R positions to the left, wrapping around when 
+  //!         getting to the beginning.
+  //!
+  //! @tparam R Rotation factor
+  //! @param t  Product type to rotate. 
+  //! @return   A product type equivalent to t with elements rotated R positions to the left.
+  //!
+  //! ## Helper type
+  //! @code
+  //! namespace kumi::result
+  //! {
+  //!   template<std::size_t R, product_type T> struct rotate_left;
+  //!
+  //!   template<std::size_t R, product_type T>
+  //!   using rotate_left_t = typename rotate_left<R, T>::type;
+  //! }
+  //! @endcode
+  //!
+  //! Computes the return type of a call to kumi::rotate_left
+  //!
+  //! ## Example
+  //! @include doc/rotate_left.cpp
+  //! @include doc/record/rotate_left.cpp
+  //================================================================================================
+  template<std::size_t R, product_type T>
+  constexpr auto rotate_left(T && t)
+  {
+    if constexpr ( sized_product_type<T,0> ) return KUMI_FWD(t);
+    else if constexpr ( (R % size_v<T> ) == 0 ) return KUMI_FWD(t);
+    else
+    {
+      constexpr auto idxs = _::rotator<size_v<T>, R % size_v<T>>();
+      return [&]<std::size_t...I>(std::index_sequence<I...>)
+      {
+        using type = _::builder_make_t<T, element_t< idxs.t[I], T > ...>;
+        return type { get<idxs.t[I]>(KUMI_FWD(t))... };
+      }(std::make_index_sequence<size_v<T>>{});
+    }
+  }
+
+  //================================================================================================
+  //! @ingroup generators 
+  //! @brief  Rotates the element of a product type R positions to the right, wrapping around when 
+  //!         getting to the end.
+  //!
+  //! @tparam R Rotation factor
+  //! @param t  Product type to rotate. 
+  //! @return   A product type equivalent to t with elements rotated R positions to the right.
+  //!
+  //! ## Helper type
+  //! @code
+  //! namespace kumi::result
+  //! {
+  //!   template<std::size_t R, product_type T> struct rotate_right;
+  //!
+  //!   template<std::size_t R, product_type T>
+  //!   using rotate_right_t = typename rotate_right<R, T>::type;
+  //! }
+  //! @endcode
+  //!
+  //! Computes the return type of a call to kumi::rotate_right
+  //!
+  //! ## Example
+  //! @include doc/rotate_right.cpp
+  //! @include doc/record/rotate_right.cpp
+  //================================================================================================
+  template<std::size_t R, product_type T>
+  constexpr auto rotate_right(T && t)
+  {
+    if constexpr ( sized_product_type<T,0> ) return KUMI_FWD(t);
+    else if constexpr ( (R % size_v<T>) == 0 ) return KUMI_FWD(t);
+    else
+    {
+      constexpr auto F = R % size_v<T>;
+      constexpr auto idxs = _::rotator<size_v<T>, size_v<T> - F>();
+      return [&]<std::size_t...I>(std::index_sequence<I...>)
+      {
+        using type = _::builder_make_t<T, element_t< idxs.t[I], T > ...>;
+        return type { get<idxs.t[I]>(KUMI_FWD(t))... };
+      }(std::make_index_sequence<size_v<T>>{});
+    }
+  }
+
+  namespace result
+  {
+    template<std::size_t R, product_type T>
+    struct rotate_left
+    {
+      using type = decltype(kumi::rotate_left<R>( std::declval<T>()) );
+    };
+
+    template<std::size_t R, product_type T>
+    struct rotate_right
+    {
+      using type = decltype(kumi::rotate_right<R>( std::declval<T>()) );
+    };
+
+    template<std::size_t R, product_type T>
+    using rotate_left_t = typename rotate_left<R,T>::type;
+
+    template<std::size_t R, product_type T>
+    using rotate_right_t = typename rotate_right<R,T>::type;
+  }
+}
---
+++ test/unit/rotate_right.cpp
@@ -0,0 +1,83 @@
+
+//==================================================================================================
+/*
+  KUMI - Compact Tuple Tools
+  Copyright : KUMI Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#define TTS_MAIN
+#include <kumi/kumi.hpp>
+#include <tts/tts.hpp>
+
+TTS_CASE("Check result::rotate_right<R,Tuple> behavior")
+{
+  using tuple_t = kumi::tuple<char,short,int,double>;
+
+  TTS_TYPE_IS( (kumi::result::rotate_right_t<0,tuple_t>), tuple_t                               );
+  TTS_TYPE_IS( (kumi::result::rotate_right_t<1,tuple_t>), (kumi::tuple<double,char,short,int>)  );
+  TTS_TYPE_IS( (kumi::result::rotate_right_t<2,tuple_t>), (kumi::tuple<int,double,char,short>)  );
+  TTS_TYPE_IS( (kumi::result::rotate_right_t<kumi::size_v<tuple_t>,tuple_t>), tuple_t           );
+  TTS_TYPE_IS( (kumi::result::rotate_right_t<1,kumi::tuple<>>), kumi::tuple<>                   );
+
+  using nested_t = kumi::tuple<kumi::tuple<int, float>, int>;
+  TTS_TYPE_IS( (kumi::result::rotate_right_t<1,nested_t>), (kumi::tuple<int,kumi::tuple<int, float>>));
+};
+
+TTS_CASE("Check rotate_right<I...>(tuple) behavior")
+{
+    auto t = kumi::tuple {1, 2., 3.4f, '5'};
+    
+    {
+      auto s = kumi::rotate_right<4>(t);
+      TTS_EQUAL(s,t);
+    }
+    {
+      auto s = kumi::rotate_right<3>(t);
+      TTS_EQUAL(s, (kumi::tuple{2.,3.4f,'5',1}));
+    }
+
+    {
+      auto s = kumi::rotate_right<2>(t);
+      TTS_EQUAL(s, (kumi::tuple{3.4f,'5',1,2.}));
+    }
+
+    {
+      auto s = kumi::rotate_right<1>(t);
+      TTS_EQUAL(s, (kumi::tuple{'5',1,2.,3.4f}) );
+    }
+    
+    {
+      auto s = kumi::rotate_right<0>(t);
+      TTS_EQUAL(s, t);
+    }
+};
+
+TTS_CASE("Check rotate_right<I...>(tuple) constexpr behavior")
+{
+    constexpr auto t = kumi::tuple {1, 2., 3.4f, '5'};
+    
+    {
+      constexpr auto s = kumi::rotate_right<4>(t);
+      TTS_CONSTEXPR_EQUAL(s,t);
+    }
+    {
+      constexpr auto s = kumi::rotate_right<3>(t);
+      TTS_CONSTEXPR_EQUAL(s, (kumi::tuple{2.,3.4f,'5',1}));
+    }
+
+    {
+      constexpr auto s = kumi::rotate_right<2>(t);
+      TTS_CONSTEXPR_EQUAL(s, (kumi::tuple{3.4f,'5',1,2.}));
+    }
+
+    {
+      constexpr auto s = kumi::rotate_right<1>(t);
+      TTS_CONSTEXPR_EQUAL(s, (kumi::tuple{'5',1,2.,3.4f}) );
+    }
+    
+    {
+      constexpr auto s = kumi::rotate_right<0>(t);
+      TTS_CONSTEXPR_EQUAL(s, t);
+    }
+};
