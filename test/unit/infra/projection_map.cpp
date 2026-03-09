3fcb4242472fa655da337c992d3e90260b6652d3
Nxirda <Nxirda@users.noreply.github.com>
2026-03-10 00:02:46 +0100

    MSVC doesnt seem to like decltype(type_instantiation) ?

--- test/unit/infra/projection_map.cpp
+++ test/unit/infra/projection_map.cpp
@@ -13,22 +13,22 @@ TTS_CASE("Check kumi::concepts::projection bejavior")
 {
   using namespace kumi::literals;
 
-  using map1 = decltype(kumi::projection_map{0_c, 1_c});
-  using map2 = decltype(kumi::projection_map{0_c, kumi::projection_map{0_c, 1_c}});
-  using map3 = decltype(kumi::indexes(0_c, 1_c));
-  using map4 = decltype(kumi::make_indexes<1, 2, 3, 4, 5>());
+  auto map1 = kumi::projection_map{0_c, 1_c};
+  auto map2 = kumi::projection_map{0_c, kumi::projection_map{0_c, 1_c}};
+  auto map3 = kumi::indexes(0_c, 1_c);
+  auto map4 = kumi::make_indexes<1, 2, 3, 4, 5>();
 
-  // using map5 = decltype(kumi::identifiers("a"_id, "b"_id));
-  // using map6 = decltype(kumi::projection_map{"a"_id, "b"_id});
+  auto map5 = kumi::identifiers("a"_id, "b"_id);
+  auto map6 = kumi::projection_map{"a"_id, "b"_id};
 
   TTS_EXPECT((kumi::concepts::projection<std::integral_constant<std::size_t, 0>>));
   TTS_EXPECT((kumi::concepts::projection<kumi::index_t<0>>));
 
-  TTS_EXPECT((kumi::concepts::projection<map1>));
-  TTS_EXPECT((kumi::concepts::projection<map2>));
-  TTS_EXPECT((kumi::concepts::projection<map3>));
-  TTS_EXPECT((kumi::concepts::projection<map4>));
+  TTS_EXPECT((kumi::concepts::projection<decltype(map1)>));
+  TTS_EXPECT((kumi::concepts::projection<decltype(map2)>));
+  TTS_EXPECT((kumi::concepts::projection<decltype(map3)>));
+  TTS_EXPECT((kumi::concepts::projection<decltype(map4)>));
 
-  // TTS_EXPECT((kumi::concepts::projection<map5>));
-  // TTS_EXPECT((kumi::concepts::projection<map6>));
+  TTS_EXPECT((kumi::concepts::projection<decltype(map5)>));
+  TTS_EXPECT((kumi::concepts::projection<decltype(map6)>));
 };
