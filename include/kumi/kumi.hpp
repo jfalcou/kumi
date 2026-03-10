3edcefae8bedb0d8e6d369cd29fa84df0a19ec5c
Nxirda <Nxirda@users.noreply.github.com>
2026-03-10 13:28:01 +0100

    Include guards inside kumi.hpp

--- include/kumi/kumi.hpp
+++ include/kumi/kumi.hpp
@@ -5,10 +5,13 @@
   SPDX-License-Identifier: BSL-1.0
 */
 //==================================================================================================
-#pragma once
+#ifndef KUMI_HPP_INCLUDED
+#define KUMI_HPP_INCLUDED
 
 #include <kumi/fwd.hpp>
 #include <kumi/detail.hpp>
 #include <kumi/utils.hpp>
 #include <kumi/product_types.hpp>
 #include <kumi/algorithm.hpp>
+
+#endif
